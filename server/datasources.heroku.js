5d0b0223cb4abfa72f38789c2fd7daa246659733
Dominik Egger <dominik@almende.org>
2015-11-27 16:12:39 +0100

    update environment variables

--- server/datasources.heroku.js
+++ server/datasources.heroku.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoDs: {
-    "connector": "mongodb",
+    "connector": "loopback-connector-mongodb",
     "host": "ds047474.mongolab.com",
     "port": 47474,
     "database": process.env.MONGODB_DATABASE,
@@ -8,7 +8,6 @@ module.exports = {
     "password": process.env.MONGODB_PASSWORD
   },
   "sendgridDs": {
-    "name": "sendgridDs",
     "connector": "loopback-connector-sendgrid",
     "api_user": process.env.SENDGRID_USERNAME,
     "api_key": process.env.SENDGRID_PASSWORD
