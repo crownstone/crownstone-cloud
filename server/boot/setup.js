12a919461a8449a4137de8de7a62fb75982ed047
Dominik Egger <dominik@dobots.nl>
2016-11-01 10:08:13 +0100

    undo setup.js

--- server/boot/setup.js
+++ server/boot/setup.js
@@ -1,21 +1,21 @@
-module.exports = function(app) {
+// module.exports = function(app) {
 
-	app.models().forEach(function(Model) {
-		console.log(Model.modelName);
+// 	app.models().forEach(function(Model) {
+// 		console.log(Model.modelName);
 
-		if (Model.modelName == "Sphere") {
-			// console.log(Model.sharedClass);
-			Model.sharedClass.methods().forEach(function(method) {
-				// if (method.name.indexOf('updateById') > 0) {
-					console.log("   " + method.name);
-				// }
-			})
-			// Model.disableRemoteMethod('updateAttributes', false);
-		}
-	})
+// 		if (Model.modelName == "Sphere") {
+// 			// console.log(Model.sharedClass);
+// 			Model.sharedClass.methods().forEach(function(method) {
+// 				// if (method.name.indexOf('updateById') > 0) {
+// 					console.log("   " + method.name);
+// 				// }
+// 			})
+// 			// Model.disableRemoteMethod('updateAttributes', false);
+// 		}
+// 	})
 
-	// app.remotes().methods().forEach(function(method) {
-	// 	comnsole.log(method.name);
-	// })
+// 	// app.remotes().methods().forEach(function(method) {
+// 	// 	comnsole.log(method.name);
+// 	// })
 
-}
+// }
