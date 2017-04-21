424e8f303aab556fc49b549ebed5165382856a50
Alex de Mulder <alexdemulder@gmail.com>
2017-04-21 16:05:46 +0200

    added sha1 hashes to the firmware

--- server/inserts/firmwareReleases.js
+++ server/inserts/firmwareReleases.js
@@ -3,27 +3,35 @@
 function performFirmwareOperations(app) {
   let firmwareModel = app.dataSources.mongoDs.getModel('Firmware');
   let bootloaderModel = app.dataSources.mongoDs.getModel('Bootloader');
-  //let userModel = app.dataSources.userDs.getModel('user');
 
   // showFirmwares(firmwareModel);
   // showBootloaders(bootloaderModel);
 
-  // releaseFirmware(
-  //   firmwareModel,
-  //   '1.3.1', // release version
-  //   '1.3.1', // minimum compatible version
-  //   '1.3.1', // minimum compatible version
-  //   'https://github.com/crownstone/bluenet-release/raw/master/crownstone_1.3.1/bin/crownstone_1.3.1.zip'
-  // );
-  //
-  // releaseBootloader(
-  //   bootloaderModel,
-  //   '1.2.2', // release version
-  //   '1.2.2', // minimum compatible version
-  //   '1.2.2', // minimum compatible version
-  //   'https://github.com/crownstone/bluenet-release/raw/master/bootloader_1.2.2/bin/bootloader_1.2.2.zip'
-  // );
+  releaseFirmware(
+    firmwareModel,
+    '1.3.1', // release version
+    '1.3.1', // minimum compatible version
+    'f4871adaef314e32b96d0004e2e77a879aa651d1', // sha1 hash to validate download
+    'https://github.com/crownstone/bluenet-release/raw/master/crownstone_1.3.1/bin/crownstone_1.3.1.zip'
+  );
+
+  releaseBootloader(
+    bootloaderModel,
+    '1.2.2', // release version
+    '1.2.2', // minimum compatible version
+    '45306bf3ed920dc9768a57c3df3fd16954ea5b97   ', // sha1 hash to validate download
+    'https://github.com/crownstone/bluenet-release/raw/master/bootloader_1.2.2/bin/bootloader_1.2.2.zip'
+  );
 }
+
+function clearFirmwares(firmwareModel) {
+  _removeAll(firmwareModel, 'FIRMWARES');
+}
+
+function clearBootloaders(bootloaderModel) {
+  _removeAll(bootloaderModel, 'BOOTLOADERS');
+}
+
 
 function showFirmwares(firmwareModel) {
   show(firmwareModel, 'Firmware');
@@ -80,6 +88,13 @@ function _remove(model, version) {
         deletionPromises.push(model.destroyById(result.id))
       });
       return Promise.all(deletionPromises);
+    })
+}
+
+function _removeAll(model, type) {
+  return model.destroyAll()
+    .then(() => {
+      console.log("REMOVED ALL", type);
     })
 }
 
