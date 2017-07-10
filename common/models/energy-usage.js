223461e07991c827578ea9b55a24571845d26a4d
Alex de Mulder <alexdemulder@gmail.com>
2017-07-10 14:07:43 +0200

    made powerUsage and EnergyUsage public models with hidden endpoints.

--- common/models/energy-usage.js
+++ common/models/energy-usage.js
@@ -2,4 +2,17 @@
 
 module.exports = function(model) {
   model.validatesUniquenessOf('timestamp', {scopedTo: ['stoneId'], message: 'there already is a data point for this stone and this exact time.'});
+
+  // disable all remote methods.
+  model.disableRemoteMethodByName('create');                     // disable POST	    api/model/
+  model.disableRemoteMethodByName('patchOrCreate');              // disable PATCH	  api/model/
+  model.disableRemoteMethodByName('findById');                   // disable GET	    api/model/:id
+  model.disableRemoteMethodByName('find');                       // disable GET	    api/model
+  model.disableRemoteMethodByName('destroyById');                // disable DELETE	  api/model/:id
+  model.disableRemoteMethodByName('deleteById');                 // disable DELETE	  api/model/:id
+  model.disableRemoteMethodByName('replaceById');                // disable PUT	    api/model/:id  and  api/model/:id/replace
+  model.disableRemoteMethodByName('prototype.patchAttributes');  // disable PATCH	  api/model/:id
+  model.disableRemoteMethodByName('prototype.__get__owner');
+  model.disableRemoteMethodByName('prototype.__get__appliance');
+  model.disableRemoteMethodByName('prototype.__get__stone');
 };
--- server/inserts/firmwareReleases.js
+++ server/inserts/firmwareReleases.js
@@ -9,30 +9,43 @@ function performFirmwareOperations(app) {
   let bootloaderModel = app.dataSources.mongoDs.getModel('Bootloader');
 
   // start with empty promise so we can put all commands in a chain (easier for commenting/uncommenting lines)
-  // new Promise((resolve, reject) => resolve())
-  //   .then(() => {
-  //     return releaseFirmware(
-  //       firmwareModel,
-  //       '1.3.3', // release version
-  //       '1.3.1', // minimum compatible version,
-  //       plugAndBuiltinVariations, // hardware versions
-  //       '46c0c972bba1a6e0215ed0ba4e24b4f1252c6dfd', // sha1 hash to validate download
-  //       'https://github.com/crownstone/bluenet-release/raw/master/crownstone_1.3.3/bin/crownstone_1.3.3.zip',
-  //       1 // release level: 0 for release to all new users
-  //     );
-  //   })
-    // .then(() => {clearFirmwares(firmwareModel) })
-    // .then(() => {clearBootloaders(bootloaderModel) })
-  //   .then(() => {
-  //     return releaseBootloader(
-  //       bootloaderModel,
-  //       '1.2.2', // release version
-  //       '1.2.2', // minimum compatible version
-  //       plugAndBuiltinVariations, // hardware versions
-  //       '45306bf3ed920dc9768a57c3df3fd16954ea5b97', // sha1 hash to validate download
-  //       'https://github.com/crownstone/bluenet-release/raw/master/bootloader_1.2.2/bin/bootloader_1.2.2.zip'
-  //     );
-  //   })
+  new Promise((resolve, reject) => resolve())
+    // .then(() => { return clearFirmwares(firmwareModel) })
+    // .then(() => { return clearBootloaders(bootloaderModel) })
+    // .then(() => {
+    //   return releaseFirmware(
+    //     firmwareModel,
+    //     '1.4.0', // release version
+    //     '1.3.1', // minimum compatible version,
+    //     plugAndBuiltinVariations, // hardware versions
+    //     'e66ccea1cac79905eee81197339ef3ff41ef7fc9', // sha1 hash to validate download
+    //     'https://github.com/crownstone/bluenet-release/raw/master/firmwares/crownstone_1.4.0/bin/crownstone_1.4.0.zip',
+    //     1 // release level: 0 for release to all new users
+    //   );
+    // })
+    // .then(() => {
+    //   return releaseBootloader(
+    //     bootloaderModel,
+    //     '1.2.2', // release version
+    //     '1.2.2', // minimum compatible version
+    //     plugAndBuiltinVariations, // hardware versions
+    //     '45306bf3ed920dc9768a57c3df3fd16954ea5b97', // sha1 hash to validate download
+    //     'https://github.com/crownstone/bluenet-release/raw/master/bootloaders/bootloader_1.2.2/bin/bootloader_1.2.2.zip',
+    //     1 // release level: 0 for release to all new users
+    //   );
+    // })
+    // .then(() => {return clearFirmwares(firmwareModel) })
+    // .then(() => {return clearBootloaders(bootloaderModel) })
+    // .then(() => {
+    //   return releaseBootloader(
+    //     bootloaderModel,
+    //     '1.2.2', // release version
+    //     '1.2.2', // minimum compatible version
+    //     plugAndBuiltinVariations, // hardware versions
+    //     '45306bf3ed920dc9768a57c3df3fd16954ea5b97', // sha1 hash to validate download
+    //     'https://github.com/crownstone/bluenet-release/raw/master/bootloader_1.2.2/bin/bootloader_1.2.2.zip'
+    //   );
+    // })
 }
 
 function clearFirmwares(firmwareModel) {
