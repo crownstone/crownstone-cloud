5bbbb3974b12e1431fd1a8cfa1b8585ecb66f9bb
Alex de Mulder <alexdemulder@gmail.com>
2017-09-20 14:57:31 +0200

    added currentSphere method to device

--- common/models/device.js
+++ common/models/device.js
@@ -56,6 +56,8 @@ module.exports = function(model) {
   // address has to be unique to a stone
   // model.validatesUniquenessOf('address', {message: 'a device with this address was already added!'});
 
+  model.disableRemoteMethodByName('prototype.__get__currentLocation');
+  model.disableRemoteMethodByName('prototype.__get__currentSphere');
   model.disableRemoteMethodByName('prototype.__create__installations');
 
   model.disableRemoteMethodByName('createChangeStream');
@@ -156,6 +158,63 @@ module.exports = function(model) {
         {arg: 'id', type: 'any', required: true, 'http': {source: 'path'}}
       ],
       description: "Set the current location of the device"
+    }
+  );
+
+  model.setCurrentSphere = function(device, sphereId, next) {
+    if ((device.currentSphereId === sphereId) ||
+      (String(device.currentSphereId) === String(sphereId))) {
+      debug("location == current location, nothing to do!");
+      return next();
+    }
+
+    let Sphere = loopback.getModel('Sphere');
+    Sphere.findById(sphereId, function(err, sphere) {
+      if (err) return next(err);
+      if (Sphere.checkForNullError(sphere, next, "id: " + sphereId)) return;
+
+      device.currentSphereId = sphereId;
+      device.save(function(err, deviceInstance) {
+        next(err);
+      })
+    });
+  };
+
+  model.clearCurrentSphere = function(device, next) {
+    debug("clearing current sphere");
+    device.currentSphereId = null;
+
+    device.save(function(err, deviceInstance) {
+      if (next) {
+        if (err) return next(err);
+        next();
+      }
+    })
+
+  };
+
+  model.remoteSetCurrentSphere = function(sphereId, deviceId, next) {
+    model.findById(deviceId, function(err, device) {
+      if (err) return next(err);
+      if (model.checkForNullError(device, next, "id: " + deviceId)) return;
+
+      if (sphereId) {
+        model.setCurrentSphere(device, sphereId, next);
+      } else {
+        model.clearCurrentSphere(device, next);
+      }
+    })
+  };
+
+  model.remoteMethod(
+    'remoteSetCurrentSphere',
+    {
+      http: {path: '/:id/currentSphere/:fk', verb: 'PUT'},
+      accepts: [
+        {arg: 'fk', type: 'any', 'http': {source: 'path'}},
+        {arg: 'id', type: 'any', required: true, 'http': {source: 'path'}}
+      ],
+      description: "Set the current Sphere of the device"
     }
   );
 
