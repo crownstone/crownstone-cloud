f0d18a2420d36a8e319ed88fd434b0e296220bd1
Alex de Mulder <alexdemulder@gmail.com>
2017-10-02 14:57:04 +0200

    added indirection to change model, it will now get the sphereId from the stone model.

--- server/mixins/change-tracker.js
+++ server/mixins/change-tracker.js
@@ -16,32 +16,72 @@ module.exports = function (model, options) {
     if (ctx.instance === undefined) {
       let deleteModel = loopback.getModel(modelName);
 
-      deleteModel.find({where: ctx.where})
-        .then((results) => {
-          let dataChanges = [];
-          for (let i = 0; i < results.length; i++) {
-            dataChanges.push({model: modelName, type: type, itemId: results[i].id, sphereId: results[i].sphereId})
-          }
+      let sphereId = null;
+      new Promise((resolve,reject) => {
 
-          if (dataChanges.length > 0) {
-            let Changes = loopback.getModel("Change");
-            return Changes.create(dataChanges)
+        // delete ALL events may not always yield the sphereId
+        let whereFilterKeys = ctx.where ? Object.keys(ctx.where) : [];
+        if (whereFilterKeys.length > 0) {
+          if (whereFilterKeys[0] === 'stoneId') {
+            return _getSphereIdFromStone(ctx.where.stoneId)
+              .then((sphereIdFound) => {
+                sphereId = sphereIdFound;
+                resolve();
+              })
+              .catch((err) => { resolve(); })
           }
-        })
-        .then(() => {
-            next();
-        })
-        .catch((err) => {
-          next(err);
-        })
+        }
+        resolve();
+      })
+      .then(() => {
+        return deleteModel.find({where: ctx.where})
+      })
+      .then((results) => {
+        let dataChanges = [];
+        for (let i = 0; i < results.length; i++) {
+          dataChanges.push({model: modelName, type: type, itemId: results[i].id, sphereId: results[i].sphereId || sphereId })
+        }
+
+        if (dataChanges.length > 0) {
+          let Changes = loopback.getModel("Change");
+          return Changes.create(dataChanges)
+        }
+      })
+      .then(() => {
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      })
     }
     else {
       // delete single item
       let itemId = ctx.instance.id;
       let sphereId = ctx.instance.sphereId;
 
-      let Changes = loopback.getModel("Change");
-      Changes.create({model: modelName, type: type, itemId: itemId, sphereId: sphereId})
+      new Promise((resolve,reject) => {
+        sphereId = ctx.instance.sphereId;
+        if (!sphereId) {
+          if (ctx.instance.stoneId) {
+            return _getSphereIdFromStone(ctx.where.stoneId)
+              .then((sphereIdFound) => {
+                sphereId = sphereIdFound;
+                resolve();
+              })
+              .catch((err) => { reject("Could not find sphereId in model with id: " + itemId); })
+          }
+          else {
+            reject("Could not find sphereId in model with id: " + itemId);
+          }
+        }
+        else {
+          resolve();
+        }
+      })
+        .then(() => {
+          let Changes = loopback.getModel("Change");
+          return Changes.create({model: modelName, type: type, itemId: itemId, sphereId: sphereId})
+        })
         .then(() => {
           next();
         })
@@ -49,6 +89,19 @@ module.exports = function (model, options) {
           next(err);
         })
     }
-  })
+  });
+
+  const _getSphereIdFromStone = function(stoneId) {
+    let stoneModel = loopback.getModel('Stone');
+    return stoneModel.findById(stoneId)
+      .then((result) => {
+        if (result !== null) {
+          return result.sphereId;
+        }
+        else {
+          throw "Stone with id " + stoneId + ' not found.'
+        }
+      })
+  }
 
 };
