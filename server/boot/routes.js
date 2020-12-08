2424f1d7f09f252c5de78f7d6d1b14e5c582d326
alexdm0 <alexdemulder@gmail.com>
2020-12-08 16:04:59 +0100

    try to improve ip detection

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -603,19 +603,19 @@ module.exports = function (app) {
 
   app.get('/ip', function(req, res) {
     res.end(JSON.stringify({
-      'x-forwarded-for': req.headers['x-forwarded-for'],
-      'X-Client-IP': req.headers['X-Client-IP'],
-      'CF-Connecting-IP': req.headers['CF-Connecting-IP'],
-      'Fastly-Client-Ip': req.headers['Fastly-Client-Ip'],
+      'x-forwarded-for': req.headers['x-forwarded-for'] || 'none',
+      'X-Client-IP': req.headers['X-Client-IP'] || 'none',
+      'CF-Connecting-IP': req.headers['CF-Connecting-IP'] || 'none',
+      'Fastly-Client-Ip': req.headers['Fastly-Client-Ip'] || 'none',
       'True-Client-Ip': req.headers['True-Client-Ip'],
-      'X-Real-IP': req.headers['X-Real-IP'],
-      'X-Cluster-Client-IP': req.headers['X-Cluster-Client-IP'],
-      'X-Forwarded': req.headers['X-Forwarded'],
-      'Forwarded': req.headers['Forwarded'],
-      'req.connection.remoteAddress':req.connection.remoteAddress,
-      'req.socket.remoteAddress':req.socket.remoteAddress,
-      'req.connection.socket.remoteAddress':req.connection.socket.remoteAddress,
-      'req.info.remoteAddress':req.info.remoteAddress,
+      'X-Real-IP': req.headers['X-Real-IP'] || 'none',
+      'X-Cluster-Client-IP': req.headers['X-Cluster-Client-IP'] || 'none',
+      'X-Forwarded': req.headers['X-Forwarded'] || 'none',
+      'Forwarded': req.headers['Forwarded'] || 'none',
+      'req.connection.remoteAddress':req.connection && req.connection.remoteAddress || 'none',
+      'req.socket.remoteAddress':req.socket && req.socket.remoteAddress || 'none',
+      'req.connection.socket.remoteAddress':req.connection && req.connection.socket && req.connection.socket.remoteAddress || 'none',
+      'req.info.remoteAddress':req.info && req.info.remoteAddress || 'none',
     }, null, 2))
   })
 
