227c435a6c49b56b9d167e1b9b3223a75345c340
alexdm0 <alexdemulder@gmail.com>
2022-12-09 14:42:28 +0100

    updated config session vars

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,29 +2,30 @@ module.exports = {
   mongoDs: {
     "connector": "loopback-connector-mongodb",
     "name": process.env.DATA_TABLE,
-    "url": "mongodb://" + process.env.MONGODB_USER + ":"
+    "url": "mongodb://" + (process.env.DATA_DB_URL || (+ process.env.MONGODB_USER + ":"
                         + process.env.MONGODB_PASSWORD + "@"
                         + process.env.MONGODB_HOST + ":"
                         + process.env.MONGODB_PORT + "/"
-                        + process.env.DATA_TABLE + "?authSource=admin&ssl=true&sslValidate=false",
+                        + process.env.DATA_TABLE + "?authSource=admin&ssl=true&sslValidate=false")),
   },
   userDs: {
     "connector": "loopback-connector-mongodb",
     "name": process.env.USER_TABLE,
-    "url": "mongodb://" + process.env.MONGODB_USER + ":"
+    "url": "mongodb://" + (process.env.USER_DB_URL || (+ process.env.MONGODB_USER + ":"
                         + process.env.MONGODB_PASSWORD + "@"
                         + process.env.MONGODB_HOST + ":"
                         + process.env.MONGODB_PORT + "/"
-                        + process.env.USER_TABLE + "?authSource=admin&ssl=true&sslValidate=false",
+                        + process.env.USER_TABLE + "?authSource=admin&ssl=true&sslValidate=false")),
   },
   filesDs: {
     "connector": "loopback-component-storage-gridfs",
     "name": process.env.FILES_TABLE,
-    "url": "mongodb://" + process.env.MONGODB_USER + ":"
+    "url": "mongodb://" + (process.env.FILES_DB_URL || (
+                          process.env.MONGODB_USER + ":"
                         + process.env.MONGODB_PASSWORD + "@"
                         + process.env.MONGODB_HOST + ":"
                         + process.env.MONGODB_PORT + "/"
-                        + process.env.FILES_TABLE + "?authSource=admin&ssl=true&sslValidate=false",
+                        + process.env.FILES_TABLE + "?authSource=admin&ssl=true&sslValidate=false")),
   },
   "sendgrid": {
     "connector": "loopback-connector-sendgrid",
